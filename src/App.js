ece12dbd2f8f20dda31c523cc5b56e7261e89960
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-09-30 11:03:21 -0700

    fixed bug by removing . after  in page-renderer.js

--- src/App.js
+++ src/App.js
@@ -10,11 +10,19 @@ import {
   Redirect,
 } from "react-router-dom";
 
+import PageRenderer from "./page-renderer";
+
 function App() {
   return (
     <Router>
       <div className="App">
         <Navigation />
+        <Switch>
+          {/* Be able to render any dynamic page, without having to maintain nav Link components */}
+          <Route path="/:page" component={PageRenderer} />
+          <Route path="/" render={() => <Redirect to="/home" />} />
+          <Route component={() => 404} />
+        </Switch>
       </div>
     </Router>
   );
