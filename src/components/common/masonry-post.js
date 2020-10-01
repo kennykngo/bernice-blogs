4fa1bba1304533db84c304fc7cd33c44e60491e4
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-09-30 19:34:18 -0700

    fixed mocks/featured.js and trending.js and added more scss styling

--- src/components/common/masonry-post.js
+++ src/components/common/masonry-post.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { categoryColors } from "./styles";
 
 export default function MasonryPost({ post, tagsOnTop }) {
   const style = {
@@ -6,6 +7,24 @@ export default function MasonryPost({ post, tagsOnTop }) {
   };
 
   return (
-    <a href={post.link} className="masonry-post overlay" style={style}></a>
+    <a href={post.link} className="masonry-post overlay" style={style}>
+      <div className="image-text">
+        <div className="tags-container">
+          {post.categories.map((tag, ind) => (
+            <span
+              key={ind}
+              className="tag"
+              style={{ backgroundColor: categoryColors[tag] }}
+            >
+              {tag.toUpperCase()}
+            </span>
+          ))}
+        </div>
+        <div>
+          <h2 className="image-title">{post.title}</h2>
+          <span className="image-date">{post.date}</span>
+        </div>
+      </div>
+    </a>
   );
 }
---
+++ src/components/common/styles.js
@@ -0,0 +1,11 @@
+const categoryColors = {
+  Food: "rgb(255,59,48)",
+  Fitness: "rgb(0,113,164)",
+  Health: "rgb(255,45,85)",
+  Vue: "rgb(52, 199,89)",
+  React: "rgb(64,156, 255)",
+  JavaScript: "rgb(255,179,64)",
+  Cloud: "rgb(175, 82,250)",
+};
+
+export { categoryColors };
